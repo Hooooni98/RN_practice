715e0ea1e2b3ce90884afd86b9690cc13b1943a0
Hooooni98 <piel07@naver.com>
2022-10-12 22:23:28 +0900

    [002] add include project skills

--- practice_project/App.js
+++ practice_project/App.js
@@ -5,16 +5,49 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Button,
   useColorScheme,
   View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>테스트테스트</Text>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigation.navigate('Profile', {name: 'Jane'})}
+      />
+    </View>
+  );
+}
+class Main extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <SafeAreaView>
-        <Text>"TEST"</Text>
-      </SafeAreaView>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     );
   }
 }
